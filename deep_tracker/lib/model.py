a68ebbb502771ae9de59a9d7b2d65e1ac4ac8429
LukasBommes <mail@lukasbommes.de>
2019-09-03 17:02:15 +0800

    still doe snot work

--- deep_tracker/lib/model.py
+++ deep_tracker/lib/model.py
@@ -1,11 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils.model_zoo as model_zoo
 import torchvision
-
-from lib.resnet_atrous import resnet18
-from lib.utils import load_pretrained_weights_to_modified_resnet
 
 
 class PropagationNetwork(nn.Module):
@@ -16,34 +12,34 @@ class PropagationNetwork(nn.Module):
         self.FIXED_BLOCKS = 1
         self.TRUNCATED = False
 
-        # load pretrained weights
-        resnet = resnet18()
-        resnet_weights = model_zoo.load_url('https://s3.amazonaws.com/pytorch/models/resnet18-5c106cde.pth')
-        load_pretrained_weights_to_modified_resnet(resnet, resnet_weights)
+        self.base = torchvision.models.resnet18(pretrained=True)
 
-        input_channels = 2
-        base = [
-            nn.Conv2d(input_channels, 64, kernel_size=7, stride=2, padding=3, bias=False),
-            resnet.bn1,
-            resnet.relu,
-            resnet.maxpool,
-            resnet.layer1,
-            resnet.relu,
-            resnet.layer2,
-            resnet.relu,
-            resnet.layer3,
-            resnet.relu,
-            resnet.layer4,
-            resnet.relu]
-        self.base = nn.Sequential(*base)
+        # change number of input channels from 3 to 2
+        #self.base.conv1.in_channels = 2
+        self.base.conv1 = nn.Conv2d(2, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 
-        # # fix some layers
+        # remove fully connected and avg pool layers
+        self.base = nn.Sequential(*list(self.base.children())[:-2])
+
+        # change stride to 1 in conv5 block
+        #self.base[5][0].conv1 = nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), dilation=2, padding=(1, 1), bias=False)
+        #self.base[6][0].conv1 = nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), dilation=2, padding=(1, 1), bias=False)
+        #del self.base[5][0].downsample
+        #del self.base[6][0].downsample
+        #del self.base[7][0].downsample
+        #self.base[5][0].conv2 = nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), dilation=2, padding=(1, 1), bias=False)
+
+        #self.conv1 = nn.Conv2d(512, 4, kernel_size=(1, 1), stride=(1, 1))
+        self.conv1 = nn.Conv2d(512, 4*self.POOLING_SIZE*self.POOLING_SIZE, kernel_size=(1, 1), stride=(1, 1), padding=0, bias=False)
+        self.pooling = nn.AvgPool2d(kernel_size=self.POOLING_SIZE, stride=self.POOLING_SIZE)
+
         # def set_bn_fix(m):
         #     classname = m.__class__.__name__
-        #     if classname.find('BatchNorm') != -1:
+        #     print(classname)
+        #     if classname.find('BatchNorm2d') != -1:
         #         for p in m.parameters(): p.requires_grad = False
-        #
-        # self.base.apply(set_bn_fix)
+
+        #self.base.apply(set_bn_fix)
 
         assert (0 <= self.FIXED_BLOCKS <= 4) # set this value to 0, so we can train all blocks
         if self.FIXED_BLOCKS >= 4: # fix all blocks
@@ -55,33 +51,10 @@ class PropagationNetwork(nn.Module):
         if self.FIXED_BLOCKS >= 1: # fix first 1 block
             for p in self.base[4].parameters(): p.requires_grad = False
 
-        self.conv1x1 = nn.Conv2d(512, 4*self.POOLING_SIZE*self.POOLING_SIZE, kernel_size=(1, 1), stride=(1, 1), padding=0, bias=False)
-        self.pooling = nn.AvgPool2d(kernel_size=self.POOLING_SIZE, stride=self.POOLING_SIZE)
+        #print(list(self.base.children())[5][0].conv1)
+        print(list(self.children()))
 
-        print([p.requires_grad for p in self.base.parameters()])
-        print(list(zip(list(self.children()), [p.requires_grad for p in self.base.parameters()])))
         self._init_weights()
-
-
-    # def train(self, mode=True):
-    #     # Override train so that the training mode is set as we want
-    #     nn.Module.train(self, mode)
-    #     if mode:
-    #         # Set fixed blocks to be in eval mode
-    #         self.base.eval()
-    #
-    #         index = [4, 6, 8, 10]
-    #         index = index[0:self.FIXED_BLOCKS]
-    #
-    #         for fix_layer in index:
-    #             self.base[fix_layer].train()
-    #
-    #         def set_bn_eval(m):
-    #             classname = m.__class__.__name__
-    #             if classname.find('BatchNorm') != -1:
-    #                 m.eval()
-    #
-    #         self.base.apply(set_bn_eval)
 
 
     def forward(self, motion_vectors, boxes_prev, num_boxes_mask):
@@ -90,22 +63,21 @@ class PropagationNetwork(nn.Module):
         #print("motion_vectors:", motion_vectors.shape)
         x = self.base(motion_vectors)
         #print("after ResNet18:", x.shape)
-        x = self.conv1x1(x)
+        x = self.conv1(x)
         #x = F.relu(x)
-        #print("after conv1", x.shape)
-        #print(x)
+        print("after conv1", x.shape)
 
         boxes_prev = self._change_box_format(boxes_prev)
         boxes_prev = boxes_prev[num_boxes_mask]
         boxes_prev = boxes_prev.view(-1, 5)
         # offset frame_idx so that it corresponds to batch index
-        boxes_prev[..., :, 0] = boxes_prev[..., :, 0] - boxes_prev[..., 0, 0]
-
+        #boxes_prev[..., :, 0] = boxes_prev[..., :, 0] - boxes_prev[..., 0, 0]
+        boxes_prev = self._frame_idx_to_batch_idx(boxes_prev)
         #print(boxes_prev)
 
         # compute ratio of input size to size of base output
         #spatial_scale = x.shape[-1] / (motion_vectors.shape)[-1]
-        x = torchvision.ops.ps_roi_pool(x, boxes_prev, output_size=(self.POOLING_SIZE, self.POOLING_SIZE), spatial_scale=8/1920)
+        x = torchvision.ops.ps_roi_pool(x, boxes_prev, output_size=(self.POOLING_SIZE, self.POOLING_SIZE), spatial_scale=0.002)
         #print(x)
         #print("after roi_pool", x.shape)
         x = self.pooling(x)
@@ -113,6 +85,15 @@ class PropagationNetwork(nn.Module):
         #print("after averaging", velocities_pred.shape)
 
         return velocities_pred
+
+
+    def _frame_idx_to_batch_idx(self, boxes):
+        """Converts unique frame_idx in first column of boxes into batch index."""
+        frame_idxs = torch.unique(boxes[:, 0])
+        for batch_idx, frame_idx in enumerate(frame_idxs):
+            idx = torch.where(boxes == frame_idx)[0]
+            boxes[idx, 0] = batch_idx
+        return boxes
 
 
     def _change_box_format(self, boxes):
@@ -139,7 +120,14 @@ class PropagationNetwork(nn.Module):
                     m.bias.data.zero_()
 
         # init the box regression conv layer
-        normal_init(self.conv1x1, 0, 0.001, self.TRUNCATED)
+        normal_init(self.conv1, 0, 0.001, self.TRUNCATED)
 
         # init the first conv layer of rcnn_base_mv
         normal_init(self.base[0], 0, 0.01, self.TRUNCATED)
+
+
+if __name__ == "__main__":
+
+    model = PropagationNetwork()
+    print([p.requires_grad for p in model.base.parameters()])
+    print(list(model.children()))
--- deep_tracker/lib/model_OTCD.py
+++ deep_tracker/lib/model_OTCD.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.utils.model_zoo as model_zoo
 import torchvision
 
 from lib.resnet_atrous import resnet18
+from lib.utils import load_pretrained_weights_to_modified_resnet
 
 
 class PropagationNetwork(nn.Module):
@@ -14,30 +16,34 @@ class PropagationNetwork(nn.Module):
         self.FIXED_BLOCKS = 1
         self.TRUNCATED = False
 
-        self.base = torchvision.models.resnet18(pretrained=True)
+        # load pretrained weights
+        resnet = resnet18()
+        resnet_weights = model_zoo.load_url('https://s3.amazonaws.com/pytorch/models/resnet18-5c106cde.pth')
+        load_pretrained_weights_to_modified_resnet(resnet, resnet_weights)
 
-        # change number of input channels from 3 to 2
-        #self.base.conv1.in_channels = 2
-        self.base.conv1 = nn.Conv2d(2, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        input_channels = 2
+        base = [
+            nn.Conv2d(input_channels, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            resnet.bn1,
+            resnet.relu,
+            resnet.maxpool,
+            resnet.layer1,
+            resnet.relu,
+            resnet.layer2,
+            resnet.relu,
+            resnet.layer3,
+            resnet.relu,
+            resnet.layer4,
+            resnet.relu]
+        self.base = nn.Sequential(*base)
 
-        # remove fully connected and avg pool layers
-        self.base = nn.Sequential(*list(self.base.children())[:-2])
-
-        # change stride to 1 in conv5 block
-        #self.base[5][0].conv1 = nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), dilation=2, padding=(1, 1), bias=False)
-        #self.base[5][0].conv2 = nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), dilation=2, padding=(1, 1), bias=False)
-
-        #self.conv1 = nn.Conv2d(512, 4, kernel_size=(1, 1), stride=(1, 1))
-        self.conv1 = nn.Conv2d(512, 4*self.POOLING_SIZE*self.POOLING_SIZE, kernel_size=(1, 1), stride=(1, 1), padding=0, bias=False)
-        self.pooling = nn.AvgPool2d(kernel_size=self.POOLING_SIZE, stride=self.POOLING_SIZE)
-
+        # # fix some layers
         # def set_bn_fix(m):
         #     classname = m.__class__.__name__
-        #     print(classname)
-        #     if classname.find('BatchNorm2d') != -1:
+        #     if classname.find('BatchNorm') != -1:
         #         for p in m.parameters(): p.requires_grad = False
-
-        #self.base.apply(set_bn_fix)
+        #
+        # self.base.apply(set_bn_fix)
 
         assert (0 <= self.FIXED_BLOCKS <= 4) # set this value to 0, so we can train all blocks
         if self.FIXED_BLOCKS >= 4: # fix all blocks
@@ -49,12 +55,34 @@ class PropagationNetwork(nn.Module):
         if self.FIXED_BLOCKS >= 1: # fix first 1 block
             for p in self.base[4].parameters(): p.requires_grad = False
 
-        #print([p.requires_grad for p in self.base.parameters()])
-
-        #print(list(self.base.children())[5][0].conv1)
-        print(list(zip(list(self.children()), [p.requires_grad for p in self.base.parameters()])))
+        self.conv1x1 = nn.Conv2d(512, 4*self.POOLING_SIZE*self.POOLING_SIZE, kernel_size=(1, 1), stride=(1, 1), padding=0, bias=False)
+        self.pooling = nn.AvgPool2d(kernel_size=self.POOLING_SIZE, stride=self.POOLING_SIZE)
 
         self._init_weights()
+
+        print([p.requires_grad for p in self.base.parameters()])
+        print(list(self.children()))
+
+
+    # def train(self, mode=True):
+    #     # Override train so that the training mode is set as we want
+    #     nn.Module.train(self, mode)
+    #     if mode:
+    #         # Set fixed blocks to be in eval mode
+    #         self.base.eval()
+    #
+    #         index = [4, 6, 8, 10]
+    #         index = index[0:self.FIXED_BLOCKS]
+    #
+    #         for fix_layer in index:
+    #             self.base[fix_layer].train()
+    #
+    #         def set_bn_eval(m):
+    #             classname = m.__class__.__name__
+    #             if classname.find('BatchNorm') != -1:
+    #                 m.eval()
+    #
+    #         self.base.apply(set_bn_eval)
 
 
     def forward(self, motion_vectors, boxes_prev, num_boxes_mask):
@@ -63,9 +91,10 @@ class PropagationNetwork(nn.Module):
         #print("motion_vectors:", motion_vectors.shape)
         x = self.base(motion_vectors)
         #print("after ResNet18:", x.shape)
-        x = self.conv1(x)
+        x = self.conv1x1(x)
         #x = F.relu(x)
         #print("after conv1", x.shape)
+        #print(x)
 
         boxes_prev = self._change_box_format(boxes_prev)
         boxes_prev = boxes_prev[num_boxes_mask]
@@ -77,7 +106,7 @@ class PropagationNetwork(nn.Module):
 
         # compute ratio of input size to size of base output
         #spatial_scale = x.shape[-1] / (motion_vectors.shape)[-1]
-        x = torchvision.ops.ps_roi_pool(x, boxes_prev, output_size=(self.POOLING_SIZE, self.POOLING_SIZE), spatial_scale=0.002)
+        x = torchvision.ops.ps_roi_pool(x, boxes_prev, output_size=(self.POOLING_SIZE, self.POOLING_SIZE), spatial_scale=8/1920)
         #print(x)
         #print("after roi_pool", x.shape)
         x = self.pooling(x)
@@ -88,7 +117,7 @@ class PropagationNetwork(nn.Module):
 
 
     def _change_box_format(self, boxes):
-        """Change format of boxes from [x, y, w, h] to [x1, y1, x2, y2]."""
+        """Change format of boxes from [idx, x, y, w, h] to [idx, x1, y1, x2, y2]."""
         boxes[..., 0] = boxes[..., 0]
         boxes[..., 1] = boxes[..., 1]
         boxes[..., 2] = boxes[..., 2]
@@ -111,7 +140,7 @@ class PropagationNetwork(nn.Module):
                     m.bias.data.zero_()
 
         # init the box regression conv layer
-        normal_init(self.conv1, 0, 0.001, self.TRUNCATED)
+        normal_init(self.conv1x1, 0, 0.001, self.TRUNCATED)
 
         # init the first conv layer of rcnn_base_mv
         normal_init(self.base[0], 0, 0.01, self.TRUNCATED)
